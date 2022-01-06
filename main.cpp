49729e3c32f7a30691f77ac51116843d3b6515bc
edzawapathas <edzawa-pathas@btslivh.eu>
2022-01-06 18:01:24 +0100

    Ajout de "Bonjour"

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: snir2g1
+ *
+ * Created on 6 janvier 2022, 17:57
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+   
+    cout << "Bonjour" << endl;
+    
+    return 0;
+}
+
+
